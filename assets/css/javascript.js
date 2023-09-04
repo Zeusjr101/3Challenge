8632ebbe8ad1f6b25ee52eb3ea7448b40cef813b
JesusUjr101 <JesusUjr101@gmail.com>
2023-09-04 17:48:35 -0400

    Update html and CSS

--- assets/css/javascript.js
+++ assets/css/javascript.js
@@ -1,13 +1,1 @@
-function generatePassword() {
-    var length = 12; // Length of the generated password
-    var charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+~`|}{[]\:;?><,./-=";
-    var password = ""; 
-  
-    for (var i = 0; i < length; i++) {
-      var randomIndex = Math.floor(Math.random() * charset.length);
-      password += charset.charAt(randomIndex);
-    } 
-  
-    document.getElementById("password").value = password;
-  }
-  
+var password=document.getElementById("password");
